428efa8fb7e520d7eb9b71f196ae31b63b89f8e4
Constantin Mateescu <costica1234@MacBook-Pro.local>
2016-04-29 01:50:44 +0100

    Progress on manipulating data structures for reading n-dimensional grid partition. WIP.

--- consistency.py
+++ consistency.py
@@ -2,7 +2,8 @@
  
 from optparse import OptionParser
 
-INPUT_FILE = "input.txt"
+import re
+import numpy as np
 
 def rescale_axis():
 	# Rescale axis so that the map [a, b]^n -> R will be converted to 
@@ -16,15 +17,19 @@ def rescale_axis():
 
 	pass
 
-def parse_input_file():
+def parse_input_file(input_file):
 	# Parse the input file. Assuming maps [a, b]^n -> R, we define the following
-	# format:
-	# 
-	#   n = domain size
+	# format (note that [<...>] are regarded as tags to aid readibility of the
+	# sections:
 	#   
-	#   followed by:
+	#   [domain]
 	#   
-	#   list of 'n' rows, each partioning the i-th coordinate axis, i = 0, n - 1
+	#
+	#   domain end-points ('a' and 'b')
+	#
+	#   followed by:   
+	#
+	#   'n' rows, each partioning the i-th coordinate axis, i = 0, n-1
 	#
 	#   followed by:
 	#   
@@ -46,7 +51,30 @@ def parse_input_file():
 	#   and that the hyper-grid partition is given in stricly increasing 
 	#   monotonic order and coves the end-points of the provided fuction domain.
 
-	pass
+	n = 4
+	nd_array = np.arange(360).reshape((3, 4, 5, 6))
+	
+	# Write the array to disk
+	with file(input_file, 'w+') as f:
+	    f.write('# Array shape: {0}\n'.format(nd_array.shape))
+	    traverse_nd_array(nd_array, f, n)
+
+
+def traverse_nd_array(nd_array, f, depth):
+	# Function which recursively traverses the n-dimentionsal array.
+	if depth == 2:
+		np.savetxt(f, nd_array, fmt='%-7.2f')
+	else:
+		for sub_nd_array in nd_array:
+			traverse_nd_array(sub_nd_array, f, depth - 1)
+
+	f.write('# End of %d depth \n' % depth)
+
+
+def read_data_from_file(input_file):
+	nd_array = np.loadtxt(input_file)
+	nd_array = nd_array.reshape((3, 4, 5, 6))
+	print nd_array
 
 def command_line_arguments():
     usage = """
@@ -60,20 +88,26 @@ def command_line_arguments():
    	is approximated by the second compenent of the pair ('g'). Furthermore, if
    	such a map exists, the script will return the least and greatest such maps,
    	that have the added property of being piece-wise linear.
+
+   	Usage:
+
+   	    python consistency.py --input_file <path_to_file>
     
     """
 
     parser = OptionParser(usage=usage)
 
-    parser.add_option("", "--domain-size", dest="domain_size",
-                      help="Specifies the size of the function domain.")
+    parser.add_option("", "--input-file", dest="input_file",
+                      help="Specifies the path to the input file.")
 
     return parser.parse_args()
 
+
 def main():
 	(options, args) = command_line_arguments()
-	
-	parse_input_file()
+
+	parse_input_file(options.input_file)
+	read_data_from_file(options.input_file)
 
 if __name__ == '__main__':
 	main()
