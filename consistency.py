Fixed major bug related to derivative information, which was not used to generate the best constraints within each hyper-rectangle. WIP. Consistency now fails with the old input generator - as expected. Need to reimplement that to guarantee consistent witnesses every time.

consistency.py
@@ -78,17 +78,14 @@ class Consistency:
 
 
     def build_constraints_from_function_info(self):
-        # c- <= h <= c+ and optimize to get the greatest lower bound and least upper bound for the
-        # height at any point in the grid. For the contour of the domain for which the previous
-        # inequality is not defined, use the function information.
+        # Construct bounds of the type: c_ij- <= h_st <= c_ij+, s = i,i+1, t = j,j+1.
         l_b_constraints = np.empty(self.no_points_per_axis)
         u_b_constraints = np.empty(self.no_points_per_axis)
         
         l_b_constraints.fill(np.float('-inf'))
         u_b_constraints.fill(np.float('inf'))
 
-        # For all the grid points that can have a neighbour, optimise the lower and upper bound
-        # constraints.
+        # Derive the constraints on the heights based on the adjacent subrectangles.
         indices_allowing_neighbours = Utils.get_grid_indices(self.no_points_per_axis, True)
 
         for grid_index in indices_allowing_neighbours:
@@ -120,27 +117,138 @@ class Consistency:
         return (coef_matrix, column_vector)
 
 
+    def get_partial_derivatives_end_points(self, dimension):
+        no_cols = 2**(dimension - 1)
+        result = np.zeros((dimension, no_cols), dtype=('int, int'))
+
+        for index, row in enumerate(result):
+            step = 2**(dimension - index - 1)
+            for j in range(step):
+                row[j][0] = j
+                row[j][1] = row[j][0] + step
+
+        for index in range(1, len(result)):
+            step = 2**(dimension - index - 1)
+            offset = 2 * step
+
+            for j in range(no_cols):
+                if j + step < no_cols:
+                    result[index][j + step][0] = result[index][j][0] + offset
+                    result[index][j + step][1] = result[index][j][1] + offset
+            
+        return result
+
+
     def build_constraints_from_derivative_info(self):
-        grid_indices_excluding_last_point \
-            = [Utils.get_grid_indices_excluding_last_point_on_axis(self.no_points_per_axis, i) \
-               for i in range(self.n)]
+        indices_allowing_neighbours = Utils.get_grid_indices(self.no_points_per_axis, True)
 
-        # Number of elements for each of the upper/lower bound column vectors.
-        no_elems_b_vec = sum([len(x) for x in grid_indices_excluding_last_point]) 
+        partial_derivatives_end_points = self.get_partial_derivatives_end_points(self.n)
+
+        print partial_derivatives_end_points
+
+        # for grid_index in indices_allowing_neighbours:
+        #     print grid_index
+
+        #     next_grid_indices = Utils.get_grid_indices_neighbours(grid_index)
+        #     print next_grid_indices
+
+        #     for ith_partial in range(self.n):
+        #         for end_points in partial_derivatives_end_points[ith_partial]:
+        #             next_index = next_grid_indices[end_points[1]]
+        #             curr_index = next_grid_indices[end_points[0]]
+
+        #             print next_index, curr_index
+        #         print "--------"
         
-        l_b_constraints = np.empty(no_elems_b_vec)
-        u_b_constraints = np.empty(no_elems_b_vec)
 
-        index = 0
+        ### NEW STUFF
+
+        no_rows_for_partial_derivatives = Utils.calculate_block_heights(self.no_points_per_axis)
+
+        # print no_rows_for_partial_derivatives
+
+        u_b_constraints = []
+        l_b_constraints = []
+
+        for ith_partial in range(self.n):
+            partial_l_b_constraints = np.empty(Utils.get_dimension_for_row_vector(
+                self.no_points_per_axis, ith_partial))
+            partial_l_b_constraints.fill(float('-inf'))
+
+            partial_u_b_constraints = np.empty(Utils.get_dimension_for_row_vector(
+                self.no_points_per_axis, ith_partial))
+            partial_u_b_constraints.fill(float('inf'))
+
+            for grid_index in indices_allowing_neighbours:
+                print grid_index
+
+                next_grid_indices = Utils.get_grid_indices_neighbours(grid_index)
+
+                for end_points in partial_derivatives_end_points[ith_partial]:
+                    next_index = next_grid_indices[end_points[1]]
+                    curr_index = next_grid_indices[end_points[0]]
+
+                    print " --> ", next_index, curr_index
+
+                    next_axis_index = next_index[ith_partial]
+                    curr_axis_index = curr_index[ith_partial]
+
+                    grid_diff = self.grid_info[ith_partial][next_axis_index] - \
+                                self.grid_info[ith_partial][curr_axis_index]
+
+                    partial_l_b_constraints[curr_index] = max(partial_l_b_constraints[curr_index], \
+                        grid_diff * self.derivative_info[grid_index][ith_partial][0])
+                    partial_u_b_constraints[curr_index] = min(partial_u_b_constraints[curr_index], \
+                        grid_diff * self.derivative_info[grid_index][ith_partial][1])
+
+                    #print curr_index 
+
+            l_b_constraints.extend(partial_l_b_constraints.flatten().tolist())
+            u_b_constraints.extend(partial_u_b_constraints.flatten().tolist())
+
+
+        # print np.array(u_b_constraints)
+        #print "------------------------------------"
+
+        ## END NEEW STUFF
+
+
+        # grid_indices_excluding_last_point \
+        #     = [Utils.get_grid_indices_excluding_last_point_on_axis(self.no_points_per_axis, i) \
+        #        for i in range(self.n)]
+
+        # # Number of elements for each of the upper/lower bound column vectors.
+        # no_elems_b_vec = sum([len(x) for x in grid_indices_excluding_last_point]) 
+        
+        # # print "no_elems_b_vec =", no_elems_b_vec
+        # # print grid_indices_excluding_last_point
+
+        # l_b_constraints_2 = np.empty(no_elems_b_vec)
+        # u_b_constraints_2 = np.empty(no_elems_b_vec)
+
+        # index = 0
        
-        for ith_partial, coords in enumerate(grid_indices_excluding_last_point):
-            for coord in coords:
-                next_index, current_index = coord[ith_partial] + 1, coord[ith_partial]
-                grid_diff = self.grid_info[ith_partial][next_index] - \
-                            self.grid_info[ith_partial][current_index]
-                l_b_constraints[index] = grid_diff * self.derivative_info[coord][ith_partial][0]
-                u_b_constraints[index] = grid_diff * self.derivative_info[coord][ith_partial][1]
-                index = index + 1
+        # for ith_partial, coords in enumerate(grid_indices_excluding_last_point):
+        #     print coords
+        #     for coord in coords:
+        #         print coord
+        #         next_index, current_index = coord[ith_partial] + 1, coord[ith_partial]
+        #         grid_diff = self.grid_info[ith_partial][next_index] - \
+        #                     self.grid_info[ith_partial][current_index]
+        #         l_b_constraints_2[index] = grid_diff * self.derivative_info[coord][ith_partial][0]
+        #         u_b_constraints_2[index] = grid_diff * self.derivative_info[coord][ith_partial][1]
+        #         index = index + 1
+
+        #         #print coord
+
+        # print np.array(u_b_constraints)
+        # print "---------------------------"
+        # print u_b_constraints_2
+        # print "---------------------------"
+        # print u_b_constraints_2 - np.array(u_b_constraints)
+
+        # import sys
+        # sys.exit()
 
         return (l_b_constraints, u_b_constraints)
 